7fb517669b314e3bab11b38bf4885b4a58031406
Alexis <holstine@umich.edu>
2025-03-31 21:32:50 -0400

    2

--- lab16.cpp
+++ lab16.cpp
@@ -3,12 +3,12 @@ int gcf(int a, int b) {
   int q = a/b;
   int r = a%b;
   a = b * q + r;
-  if (r == 0) {
-    return b;
-  }
-  else {
+  if (r != 0) {
     a = b;
     b = q;
     q = r;
+  }
+  else {
+    return b;
   }
 }
